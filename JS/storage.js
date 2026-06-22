26ea1174bac256fae5206db8dffdc8dd6ecd6f6f
Biel Machado <135059102+GMiranda0432@users.noreply.github.com>
2026-06-22 17:52:42 +0000

    C DE CREATE!!!

---
+++ JS/storage.js
@@ -0,0 +1,42 @@
+const personaForm = document.getElementById("personaForm");
+
+const inNomeP = document.getElementById("inNomeP");
+const inClasseP = document.getElementById("inClasseP");
+const inRacaP = document.getElementById("inRacaP");
+const inSexoP = document.getElementById("inSexoP");
+const inDescP = document.getElementById("inDescP");
+const inBioP = document.getElementById("inBioP");
+
+const inVidaP = document.getElementById("inVidaP");
+const inDanoP = document.getElementById("inDanoP");
+const inDefP = document.getElementById("inDefP");
+const inSorteP = document.getElementById("inSorteP");
+const inVeloP = document.getElementById("inVeloP");
+
+let vetPersonas = JSON.parse(localStorage.getItem("personagens")) || [];
+
+personaForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let persona = {
+        id: Date.now(),
+        nome: inNomeP.value,
+        classe: inClasseP.value,
+        raca: inRacaP.value,
+        sexo: inSexoP.value,
+        descricao: inDescP.value,
+        biografia: inBioP.value,
+        vida: Number(inVidaP.value),
+        dano: Number(inDanoP.value),
+        defesa: Number(inDefP.value),
+        sorte: Number(inSorteP.value),
+        velocidade: Number(inVeloP.value)
+
+    };
+    vetPersonas.push(persona);
+    localStorage.setItem(
+        "personagens",
+        JSON.stringify(vetPersonas)
+    );
+    alert("Personagem salvo com sucesso Monamu!");
+    personaForm.reset();
+});
